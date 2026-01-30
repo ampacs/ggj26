e09c0d4e4b498f4d8d8d5b138e3217cdb9f72bfc
Yosri Jerbi <yosri.jerbi1@gmail.com>
2026-01-30 22:01:14 +0100

    Basic boring movement

--- scenes/player_3d.gd
+++ scenes/player_3d.gd
@@ -1,4 +1,51 @@
 extends CharacterBody3D
 
+@export var playerId := "player1"
+
 @export_group("Camera")
 @export_range(0.0, 1.0) var mouse_sensitivity := 0.25
+@export var tilt_upper_limit := PI / 3.0
+@export var tilt_lower_limit := -PI / 6.0
+
+@export_group("Movement")
+@export var move_speed := 8.0
+@export var acceleration := 20.0
+
+var _camera_input_direction := Vector2.ZERO
+
+@onready var _camera_pivot: Node3D = $CameraPivot
+@onready var _camera: Camera3D = $CameraPivot/Camera3D
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_motion := (
+		event is InputEventMouseMotion and
+		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	if is_camera_motion:
+		_camera_input_direction = (event as InputEventMouseMotion).screen_relative * mouse_sensitivity
+
+
+func _physics_process(delta: float) -> void:
+	_camera_pivot.rotation.x += _camera_input_direction.y * delta
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, tilt_lower_limit, tilt_upper_limit)
+	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
+
+	_camera_input_direction = Vector2.ZERO
+	
+	var raw_input := Input.get_vector("move_left_%s" % [playerId], "move_right_%s" % [playerId], "move_up_%s" % [playerId], "move_down_%s" % [playerId])
+	var forward := _camera.global_basis.z
+	var right := _camera.global_basis.x
+	var move_direction := forward * raw_input.y + right * raw_input.x
+	move_direction.y = 0.0
+	move_direction = move_direction.normalized()
+	
+	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	move_and_slide()

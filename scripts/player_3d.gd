772df8edcda37592bfc560ace800d84d76780dd1
André Santiago <andre.mpc.santiago@gmail.com>
2026-01-31 20:16:22 +0100

    feat: add disabled functionality to handle collisions in legacy player controller

--- scripts/player_3d.gd
+++ scripts/player_3d.gd
@@ -48,7 +48,7 @@ func _physics_process(delta: float) -> void:
 		"look_up_%s" % [playerId],
 		"look_down_%s" % [playerId]
 	)
-	
+
 	if stick.length() < stick_deadzone:
 		stick = Vector2.ZERO
 	else:
@@ -56,9 +56,9 @@ func _physics_process(delta: float) -> void:
 
 	if invert_y:
 		stick.y = -stick.y
-	
+
 	_camera_input_direction += stick * stick_look_speed
-		
+
 	_camera_pivot.rotation.x += _camera_input_direction.y * delta
 	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, tilt_lower_limit, tilt_upper_limit)
 	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
@@ -85,9 +85,36 @@ func _physics_process(delta: float) -> void:
 	var is_starting_jump := Input.is_action_just_pressed("jump_%s" % [playerId]) and is_on_floor()
 	if is_starting_jump:
 		velocity.y += jump_impulse
-	
-	move_and_slide()
-	
+
+#	var collided := move_and_slide()
+#	if collided:
+#		const mass := 15.
+#		for i in get_slide_collision_count():
+#			var collision := get_slide_collision(i)
+#			var collider := collision.get_collider()
+#			if collider is RigidBody3D:
+#				var rigidbody := collider as RigidBody3D
+#
+#				var normal := collision.get_normal()
+#				var relative_velocity := self.velocity - rigidbody.linear_velocity
+#
+#				const elasticity := 1.
+#				var force := -(1. + elasticity) * (relative_velocity.dot(normal) / (1. / mass + 1. / rigidbody.mass))
+#
+#				self.velocity += (normal * force) / mass
+#				rigidbody.apply_impulse()
+#				# Objects with more mass than us should be harder to push. But doesn't really make sense to push faster than we are going
+#				var mass_ratio: float = min(1., MY_APPROX_MASS_KG / rigidbody.mass)
+#				# Optional add: Don't push object at all if it's 4x heavier or more
+#				if mass_ratio < 0.25:
+#					continue
+#				# Don't push object from above/below
+#				push_dir.y = 0
+#				# 5.0 is a magic number, adjust to your needs
+#				var push_force := mass_ratio # * 5.0
+#				rigidbody.apply_impulse(push_dir * velocity_diff_in_push_dir * push_force, collision.get_position() - rigidbody.global_position)
+#				print("applying force %f" % (push_dir.length() * velocity_diff_in_push_dir * push_force))
+
 	if move_direction.length() > 0.2:
 		_last_movement_direction = move_direction
 	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)

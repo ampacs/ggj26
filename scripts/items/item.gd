b3a145e23c97bc18cabaa93d723f3577cd7c21e6
André Santiago <andre.mpc.santiago@gmail.com>
2026-01-31 11:55:14 +0100

    feat: add actor and item functionality

---
+++ scripts/items/item.gd
@@ -0,0 +1,19 @@
+class_name Item extends Node3D
+
+@export var components: Array[ItemComponent]
+
+func get_component(type: Variant) -> ItemComponent:
+	for component in components:
+		if is_instance_of(component, type):
+			return component
+
+	return null
+
+func get_components(type: Variant) -> Array[ItemComponent]:
+	var selectedComponents: Array[ItemComponent] = [] 
+
+	for component in components:
+		if is_instance_of(component, type):
+			selectedComponents.append(component)
+
+	return selectedComponents
---
+++ scripts/items/item_mask_component.gd
@@ -0,0 +1,1 @@
+class_name ItemMaskComponent extends ItemComponent

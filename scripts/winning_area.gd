c294f58cca1614c0aada6f53a7966af53dff9881
Ana Nishimoto <ana.nishimoto@gmail.com>
2026-01-31 18:24:44 +0100

    feat: add base winning area

---
+++ scripts/winning_area.gd
@@ -0,0 +1,7 @@
+extends Area3D
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	print('touched winning area')
+	if PlayerStatus.has_debuff("mask"):
+		print('you won!!')

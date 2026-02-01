2c2cb955ec90cb1d6ed7bc8dc4327c40f3a3ca0b
Ana Nishimoto <ana.nishimoto@gmail.com>
2026-02-01 03:13:28 +0100

    feat: add victory sound effect

--- scripts/winning_area.gd
+++ scripts/winning_area.gd
@@ -1,9 +1,12 @@
 extends Area3D
 
+@onready var victory_sfx: AudioStreamPlayer = $"../victorySfx"
+
 signal game_won
 
 func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body is PlayerController:
 		var player := body as PlayerController
 		if player.hasMaskEquipped:
+			self.victory_sfx.play()
 			emit_signal("game_won", player.playerId)
